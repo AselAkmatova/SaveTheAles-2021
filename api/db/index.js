5c43ad4cc3909dc51c4847a22050db72b54db895
AselAkmatova <64733360+AselAkmatova@users.noreply.github.com>
2022-02-16 12:57:10 +0600

    Delete api directory

--- api/db/index.js
+++
@@ -1,6 +0,0 @@
-const low = require('lowdb')
-const path = require('path')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync(path.resolve(`${__dirname}/db.json`))
-
-module.exports = low(adapter)
